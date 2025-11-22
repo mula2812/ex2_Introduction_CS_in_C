715221fdfff25dde6acc89f6a9eff1ca2139da15
mula2812 <54980713+mula2812@users.noreply.github.com>
2025-11-22 19:52:53 +0200

    fixing task 2 and improve all the code according to the coding style

--- ex2.c
+++ ex2.c
@@ -1,53 +1,50 @@
 /******************
-Name:
-ID:
-Assignment:
+Name:Ilan Mulakandov
+ID:325842847
+Assignment: 2
 *******************/
 
 #include <stdio.h>
 
 int main()
 {
-    // TASK 1: Ducky's Unity Game
-
-    // TASK 2: The Memory Game
-
-    // TASK 3: Professor Pat's Power Calculation
-
-    // TASK 4: The Duck Parade
-
-    // TASK 5: The Mystery of the Repeated Digits
-
-    // TASK 6: EXIT
-
-    // dont forget through all the necessary parts to validate invalid input.
+    // VARIABLES DECLARATION
     char userChoice = 0;
-    const int decimalValueOfCharZero = 48;
-    int userChoiceNumber;
+    const int decimalValueOfCharZero = '0';
+    int userChoiceNumber=0;
+    int nextChar=0;
 
     // task one vars
-    int taskOneChoosingNum=0, taskOneHelpInt=0, sumOfOnesForTaskOne=0 ;
+    int taskOneChoosingNum=0;
+    int taskOneHelpInt=0;
+    int sumOfOnesForTaskOne=0;
 
     // task two vars
     int taskTwoDucksNum=0;
-    long long taskTwoSaveBitsNum=0, taskTwoExtractBitsNum=0;
+    int isQuackBit=0;
+    int userChoiceQuackOrSh=0;
+    unsigned long long taskTwoBitsNum=0;
 
     // task three vars
-    int taskThreeBaseNum=0, taskThreeExponentNum=0, taskThreeResultNum=1;
+    int taskThreeBaseNum=0;
+    int taskThreeExponentNum=0;
+    int taskThreeResultNum=1;
 
     // task four vars
     int taskFourDucksNum=0;
-    const int taskFourmaxDucksInARow=10;
+    const int taskFourMaxDucksInARow=10;
 
     // task five vars
-    int taskFiveInputNum=0, newNumbers=0;
-    int taskFiveHelpVarForSavingInput=0,taskFivehowManyDigitInTheInput=0, helpNewNumbers=0;
-    int currentDigit=0, currentDigitPlaceValue=0;
-    bool isDigitInNewNumbers;
+    int taskFiveInputNum=0;
+    int taskFiveHelpVarForSavingInput=0;
+    int taskFiveHowManyDigitInTheInput=0;
+    int currentDigit=0;
+    const int decimalBase=10;
+
     // MAIN MENU LOOP
     do
     {
-        printf("Welcome to our games, plase choose an option\n");
+        printf("Welcome to our games, please choose an option\n");
         printf("1. Ducky's Unity Game\n");
         printf("2. The Memory Game\n");
         printf("3. Professor Pat's Power Calculation\n");
@@ -56,17 +53,15 @@ int main()
         printf("6. Good Night Ducks\n");
 
         
-        scanf(" %C", &userChoice);
+        scanf(" %c", &userChoice);
 
         // until we don't get a valid char number from '0' to '9'
-        int nextChar = getchar();
-        if (userChoice >= '0' && userChoice <= '6'){
-            if(nextChar!= '\n' && nextChar != EOF){
-                while (nextChar != '\n' && nextChar != EOF) {
-                    nextChar = getchar(); 
-                }
-                userChoice = ' '; // reset userChoice to an invalid value
+        nextChar = getchar();
+        if(nextChar!= '\n' && nextChar != EOF){
+            while (nextChar != '\n' && nextChar != EOF) {
+                nextChar = getchar(); 
             }
+            userChoice = ' '; // reset userChoice to an invalid value
         }
 
         // Transform the char value to decimal according to the value of the char 0
@@ -107,7 +102,10 @@ int main()
 
             // TASK 2: The Memory Game
             case 2:
-
+                
+                // resetting for future use
+                taskTwoBitsNum=0; 
+                
                 printf("please enter the number of ducks:\n");
                 scanf(" %d",&taskTwoDucksNum);
                 printf("you entered %d ducks\n", taskTwoDucksNum);
@@ -118,47 +116,42 @@ int main()
                     scanf(" %d", &taskTwoDucksNum);
                 }
 
-                int userChoiceQuackSh;
-
                 // getting the quack/shout choices from the user
-                for(int i=1; i<=taskTwoDucksNum; i++){
-                    printf("duck %d do QUAK? 1 for yes, 0 for no\n", i);
-                    scanf(" %d", &userChoiceQuackSh);
-                    while(userChoiceQuackSh!=1 && userChoiceQuackSh!=0){
+                for(int i=0; i<taskTwoDucksNum; i++){
+                    printf("duck %d do QUAK? 1 for yes, 0 for no\n", i+1);
+                    scanf(" %d", &userChoiceQuackOrSh);
+                    while(userChoiceQuackOrSh!=1 && userChoiceQuackOrSh!=0){
                         printf("Invalid number, please try again\n");
-                        scanf(" %d", &userChoiceQuackSh);
+                        scanf(" %d", &userChoiceQuackOrSh);
                     }
-                    taskTwoSaveBitsNum|=userChoiceQuackSh<<i; //Check this!!!!!!!!!!!!!!!!!!!!
+                    if(userChoiceQuackOrSh){
+                        taskTwoBitsNum|=(unsigned long long)userChoiceQuackOrSh<<i; //Check this!!!!!!!!!!!!!!!!!!!!
+                    }
                 }
-                
-                // extracting one bit because their is an extra 0 at the start
-                // that added because the shifting in the end of the loop  
-                taskTwoExtractBitsNum = taskTwoSaveBitsNum>>1;//Check this!!!!!!!!!!!!!!!!!!!!
-                
+                                
                 // printing the quack/shout choices
-                for(int i=1; i<=taskTwoDucksNum; i++){
+                for(int i=0; i<taskTwoDucksNum; i++){
                     
                     // checking if the last bit is 1 or 0 using &
-                    int checkQuackBit = taskTwoExtractBitsNum & 1;
+                    isQuackBit = (taskTwoBitsNum >> i) & 1;
                     
-                    printf("%lld: \n", taskTwoSaveBitsNum);
-
                     // printing according to the extracted bit
-                    if(checkQuackBit){
-                        printf("duck number %d do Quak\n", i);
+                    if(isQuackBit){
+                        printf("duck number %d do Quak\n", i+1);
                     }
                     else{
-                        printf("duck number %d do Sh...\n", i);
+                        printf("duck number %d do Sh...\n", i+1);
                     }
-                    
-                    // moving 1 bit to right for extract the next bit
-                    taskTwoExtractBitsNum = taskTwoExtractBitsNum >> 1;
                 }
 
                 break;
 
             // TASK 3: Professor Pat's Power Calculation
             case 3:
+
+                // resetting for future use
+                taskThreeResultNum=1;
+
                 printf("please enter the number\n");
                 scanf(" %d", &taskThreeBaseNum);
 
@@ -198,47 +191,52 @@ int main()
                 }
                 
                 // until we have more ducks than the max in a row
-                while (taskFourDucksNum > taskFourmaxDucksInARow){
+                while (taskFourDucksNum > taskFourMaxDucksInARow){
                     // print a full row (10 ducks) heades
-                    for(int i=0; i<taskFourmaxDucksInARow; i++){
-                        printf("   _        ");
+                    for(int i=0; i<taskFourMaxDucksInARow; i++){
+                        printf("   _\t\t");
                     }
                     printf("\n");
 
                     // print a full row (10 ducks) beaks
-                    for(int i=0; i<taskFourmaxDucksInARow; i++){
-                        printf("__(o)>      ");
+                    for(int i=0; i<taskFourMaxDucksInARow; i++){
+                        printf("__(o)>\t\t");
                     }
 
                     // print a full row (10 ducks) bodies
                     printf("\n");
-                    for(int i=0; i<taskFourmaxDucksInARow; i++){
-                        printf("\\___)       ");
+                    for(int i=0; i<taskFourMaxDucksInARow; i++){
+                        printf("\\___)\t\t");
                     }
                     printf("\n");
 
                     // decrease the number of ducks left to print
-                    taskFourDucksNum -= taskFourmaxDucksInARow;
+                    taskFourDucksNum -= taskFourMaxDucksInARow;
                 }
 
                 // print the last row with the remaining ducks
 
                 for(int i=0; i<taskFourDucksNum; i++){
-                    printf("   _        ");
+                    printf("   _\t\t");
                 }
                 printf("\n");
                 for(int i=0; i<taskFourDucksNum; i++){
-                    printf("__(o)>      ");
+                    printf("__(o)>\t\t");
                 }
                 printf("\n");
                 for(int i=0; i<taskFourDucksNum; i++){
-                    printf("\\___)       ");
+                    printf("\\___)\t\t");
                 }
                 printf("\n");
                 break;
 
             // TASK 5: The Mystery of the Repeated Digits
             case 5:
+
+                // resetting for future use
+                taskFiveHowManyDigitInTheInput=0;
+                taskFiveInputNum=0;
+
                 printf("please enter number:\n");
                 scanf(" %d", &taskFiveInputNum);
                 
@@ -252,24 +250,27 @@ int main()
                 taskFiveHelpVarForSavingInput=taskFiveInputNum;
                 
                 // counting how many digits in the input number
-                // and saving it in taskFivehowManyDigitInTheInput
+                // and saving it in taskFiveHowManyDigitInTheInput
                 while(taskFiveHelpVarForSavingInput>0){
-                    taskFivehowManyDigitInTheInput+=1;
-                    taskFiveHelpVarForSavingInput= taskFiveHelpVarForSavingInput/10;
+                    // counting one more digit
+                    taskFiveHowManyDigitInTheInput+=1;
+
+                    // cutting the last digit
+                    taskFiveHelpVarForSavingInput= taskFiveHelpVarForSavingInput/decimalBase;
                 }
 
-
+                // resetting for future use
                 taskFiveHelpVarForSavingInput=taskFiveInputNum;
                 
                 // checking for each digit if it appears more than once
                 // acording to orders
-                for(int i=0;i<taskFivehowManyDigitInTheInput;i++){
-                    currentDigit=taskFiveHelpVarForSavingInput%10;
+                for(int i=0;i<taskFiveHowManyDigitInTheInput;i++){
+                    currentDigit=taskFiveHelpVarForSavingInput%decimalBase;
                     
                     // checking if we have the same digit in the rest of the digits
                     while(taskFiveHelpVarForSavingInput>0){
                         
-                        taskFiveHelpVarForSavingInput= taskFiveHelpVarForSavingInput/10;
+                        taskFiveHelpVarForSavingInput= taskFiveHelpVarForSavingInput/decimalBase;
                         
                         // if we reach 0 it means we checked all the digits
                         if(taskFiveHelpVarForSavingInput==0){
@@ -277,7 +278,7 @@ int main()
                         }
                         
                         // if we found the same digit we print and break
-                        if(currentDigit==taskFiveHelpVarForSavingInput%10){
+                        if(currentDigit==taskFiveHelpVarForSavingInput%decimalBase){
                             printf("%d appears more than once!\n", currentDigit);
                             break;
                         }
@@ -285,18 +286,18 @@ int main()
 
                     // preparing for the next digit check
                     // cutting the last digit that we just checked and saving the rest
-                    taskFiveInputNum=taskFiveInputNum/10;
+                    taskFiveInputNum=taskFiveInputNum/decimalBase;
                     taskFiveHelpVarForSavingInput=taskFiveInputNum;
                 }
                 break;
             // TASK 6: EXIT
             case 6:
-                printf("Good night! see you at the pond tomorrow.");
+                printf("Good night! See you at the pond tomorrow.");
                 break;
 
             // INVALID INPUT HANDLING
             default:
-                printf("Invalid number, please try again\n");
+                printf("Invalid option, please try again\n");
                 break;
         }
     }
